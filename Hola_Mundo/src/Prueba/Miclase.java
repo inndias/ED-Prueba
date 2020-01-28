97dacb71b20444c3627b5220c8af96ac289583f4
inndias <60153324+inndias@users.noreply.github.com>
2020-01-28 20:23:17 +0100

    Update Miclase.java

--- Hola_Mundo/src/Prueba/Miclase.java
+++ Hola_Mundo/src/Prueba/Miclase.java
@@ -4,7 +4,7 @@ public class Miclase {
 public static void main(String [] args) {
 	System.out.println("hola mundo");
 	int tonto=5;
-	System.out.println("david no sabe y le enseñamos");
+	System.out.println("david no sabe y le enseÃ±amos menos unai que tampoco sabe");
 }
 }
 
