24138c6cbc971de66e9c3f3a7158e288f60f26d3
1b_dam20_10 <1b_dam20_10@A22_PC11.JOVELLANOS.EDU>
2020-01-28 20:18:12 +0100

    hola

---
+++ Hola_Mundo/src/Prueba/Miclase.java
@@ -0,0 +1,10 @@
+package Prueba;
+
+public class Miclase {
+public static void main(String [] args) {
+	System.out.println("hola mundo");
+	int tonto=5;
+	System.out.println("david no sabe y le enseñamos");
+}
+}
+
